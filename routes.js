720e7a1edf7e5aebb8e67e69187cc5d1f38dc317
Andrey Deynega <andrtomsk@narod.ru>
2021-01-15 15:09:38 +0700

    second commit

--- routes.js
+++ routes.js
@@ -36,15 +36,6 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-// module.exports = requestHandler;
-
-// module.exports = {
-//     handler: requestHandler,
-//     someText: 'Some hard coded text'
-// };
-
-// module.exports.handler = requestHandler;
-// module.exports.someText = 'Some text';
 
 exports.handler = requestHandler;
 exports.someText = 'Some hard coded text';
