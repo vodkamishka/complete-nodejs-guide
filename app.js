f382220ee3bc61871eb4c8548ea26e9b87d48238
Andrey Deynega <andrtomsk@narod.ru>
2021-01-18 19:13:30 +0700

    add pug

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(3000);
